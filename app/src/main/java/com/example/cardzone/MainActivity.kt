38a17c8331776676f3a6f052d36ab1d7472dd573
Emil Åkerman <emil@emils-air.lan>
2022-09-21 16:55:37 +0200

    front design, quit button functionality

--- app/src/main/java/com/example/cardzone/MainActivity.kt
+++ app/src/main/java/com/example/cardzone/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.cardzone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var quitButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //QUIT BUTTON
+        quitButton = findViewById(R.id.quitButton)
+        quitButton.setOnClickListener { finishAffinity() }
 
 
 
