e84688aafb4275e79818cec6f884c02663447d85
Emil Åkerman <emil@Emils-MacBook-Air.local>
2022-09-22 12:02:57 +0200

    work on moving cards

---
+++ app/src/main/java/com/example/cardzone/GameActivity1.kt
@@ -0,0 +1,101 @@
+package com.example.cardzone
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.graphics.Path
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.graphics.rotationMatrix
+import kotlin.random.Random
+
+class GameActivity1 : AppCompatActivity() {
+
+    lateinit var aceOfSpades : ImageView
+    lateinit var aceOfClovers : ImageView
+    lateinit var aceOfDiamonds : ImageView
+    lateinit var aceOfHearts : ImageView
+    var points = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game1)
+        supportActionBar?.hide()//REMOVES THE TOP ACTION BAR
+
+        aceOfClovers = findViewById(R.id.aceOfClovers)
+        aceOfClovers.setOnClickListener {
+            move()
+            aceOfClovers.animate().rotationBy(360f)
+            points++
+            val points2 = findViewById<TextView>(R.id.points)
+            points2.text = points.toString()
+            when (points) {
+                -1 -> finish()
+            }
+        }
+        aceOfSpades = findViewById(R.id.aceOfSpades)
+        aceOfSpades.setOnClickListener {
+            move()
+            aceOfSpades.animate().rotationBy(360f)
+            points++
+            val points2 = findViewById<TextView>(R.id.points)
+            points2.text = points.toString()
+            when (points) {
+                -1 -> finish()
+            }
+        }
+        aceOfDiamonds = findViewById(R.id.aceOfDiamonds)
+        aceOfDiamonds.setOnClickListener {
+            move()
+            aceOfHearts.animate().rotationBy(360f)
+            points--
+            val points2 = findViewById<TextView>(R.id.points)
+            points2.text = points.toString()
+            when (points) {
+                -1 -> finish()
+            }
+        }
+        aceOfHearts = findViewById(R.id.aceOfHearts)
+        aceOfHearts.setOnClickListener {
+            move()
+            aceOfHearts.animate().rotationBy(360f)
+            points--
+            val points2 = findViewById<TextView>(R.id.points)
+            points2.text = points.toString()
+            when (points) {
+                -1 -> finish()
+            }
+        }
+    }
+    fun randomPosition() = Random.nextInt(-500, 500).toFloat()
+    fun move() {
+        aceOfSpades.animate()
+            .translationX(randomPosition())
+            .translationY(randomPosition())
+            .setDuration(500)
+            .withEndAction(::move) //IF YOU REMOVE THIS IT WILL ONLY MOVE ONCE PER CLICK
+            .start()
+        aceOfClovers.animate()
+            .translationX(randomPosition())
+            .translationY(randomPosition())
+            .setDuration(500)
+            .withEndAction(::move) //IF YOU REMOVE THIS IT WILL ONLY MOVE ONCE PER CLICK
+            .start()
+        aceOfHearts.animate()
+            .translationX(randomPosition())
+            .translationY(randomPosition())
+            .setDuration(500)
+            .withEndAction(::move) //IF YOU REMOVE THIS IT WILL ONLY MOVE ONCE PER CLICK
+            .start()
+        aceOfDiamonds.animate()
+            .translationX(randomPosition())
+            .translationY(randomPosition())
+            .setDuration(500)
+            .withEndAction(::move) //IF YOU REMOVE THIS IT WILL ONLY MOVE ONCE PER CLICK
+            .start()
+    }
+}
--- app/src/main/java/com/example/cardzone/MainActivity.kt
+++ app/src/main/java/com/example/cardzone/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.cardzone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var quitButton : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()//REMOVES THE TOP ACTION BAR
 
-        //QUIT BUTTON
-        quitButton = findViewById(R.id.quitButton)
+        //PLAY BUTTON
+        val playButton = findViewById<Button>(R.id.playButton)
+        playButton.setOnClickListener {
+            val intent = Intent(this, GameActivity1::class.java)
+            startActivity(intent)
+        }
+        //QUIT BUTTON - ta bort det här, appar har inga quit-knappar
+        val quitButton = findViewById<Button>(R.id.quitButton)
         quitButton.setOnClickListener { finishAffinity() }
 
 
