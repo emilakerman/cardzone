ffa7a782b406a2265ce799c2b680748211121805
Emil Åkerman <emil@emils-air.lan>
2022-09-23 15:22:22 +0200

    made lose/win activities for game1 level2

--- app/src/main/java/com/example/cardzone/GameActivity1Level2LoseScreen.kt
+++ app/src/main/java/com/example/cardzone/GameActivity1Level2LoseScreen.kt
@@ -1,12 +1,35 @@
 package com.example.cardzone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GameActivity1Level2LoseScreen : AppCompatActivity() {
+
+
+    lateinit var backtoGameButton2 : Button
+    lateinit var mainMenuButton2 : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_activity1_level2_lose_screen)
         supportActionBar?.hide()//REMOVES THE TOP ACTION BAR
+
+        backtoGameButton2 = findViewById(R.id.backtoGameButton2)
+        backtoGameButton2.setOnClickListener {
+            val tryAgain = Intent(this, GameActivity1Level2::class.java)
+            startActivity(tryAgain)
+        }
+
+
+        mainMenuButton2 = findViewById(R.id.mainMenuButton2)
+        mainMenuButton2.setOnClickListener {
+            val mainMenu = Intent(this, MainActivity::class.java)
+            startActivity(mainMenu)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/cardzone/GameActivity1Level2WinScreen.kt
+++ app/src/main/java/com/example/cardzone/GameActivity1Level2WinScreen.kt
@@ -1,12 +1,24 @@
 package com.example.cardzone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GameActivity1Level2WinScreen : AppCompatActivity() {
+
+    lateinit var mainMenuButton4 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_activity1_level2_win_screen)
         supportActionBar?.hide()//REMOVES THE TOP ACTION BAR
+
+        mainMenuButton4 = findViewById(R.id.mainMenuButton4)
+        mainMenuButton4.setOnClickListener {
+            val mainMenu = Intent(this, MainActivity::class.java)
+            startActivity(mainMenu)
+        }
+
     }
 }
